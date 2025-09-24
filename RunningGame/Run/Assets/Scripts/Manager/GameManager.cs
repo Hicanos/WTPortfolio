938f9a48fbcf44a3b9b0246a1dea8429a21d428f
Hicanos <ruda32168@gmail.com>
2025-09-24 23:28:21 +0900

    [Feat] 이동한다
    이동한다
    그러나 점프가 안된다(아직)
    Rigidbody위치를 조정해가며 점프가 가능하도록 만들 것

---
+++ RunningGame/Run/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public int score;
+    [SerializeField] private int highScore;
+    [SerializeField] private Player player;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        //UI 활성화, UI 버튼을 눌러야 StartGame 실행
+        //현재는 바로 게임시작
+        StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        player.SetGameStart();
+    }
+
+    public void GameOver()
+    {
+        //PlayerPrefs로 가장 높은 점수 저장
+        
+        PlayerPrefs.SetInt("HighScore", Mathf.Max(score, PlayerPrefs.GetInt("HighScore", 0)));
+    }
+}
